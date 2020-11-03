7bfb3b50bc1b2f7077f9c40ca4ab3d9a7c58e3ab
shusnain <sibgatulhusnain@gmail.com>
2020-11-03 01:34:16 -0500

    Added the ability to record videos and save to cache

--- app/components/CaptureButton.js
+++ app/components/CaptureButton.js
@@ -1,35 +1,76 @@
 import React, { Component } from 'react';
-import { StyleSheet, TouchableHighlight } from 'react-native';
+import { StyleSheet, TouchableHighlight, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 class CaptureButton extends Component{
 
-	render(){
+	constructor(){
+	    super();
+	    this.state = {
+	      recording: false
+    	}
+  	}
 
+  	captureButtonInnerStyle(){
+  		var captureButtonInnerColor = 'white';
+  		if (!this.state.recording){
+  			captureButtonInnerColor = 'white'
+  		} else{
+  			captureButtonInnerColor = 'red'
+  		}
+  		return{
+  			height: 55, 
+		    width: 55,
+		    borderRadius: 50,
+		    backgroundColor: captureButtonInnerColor
+  		}
+  	}
+
+	onPress(){
+		this.props.onPress()
+		this.setState({
+			recording: !this.state.recording
+		})
+	}
+
+	render(){
 		return(
-			<TouchableHighlight
-	            activeOpacity={0.6}
-	            style={{
+			<View
+			style={{
 	              flex: 1,
 	              alignSelf: 'flex-end',
 	              alignItems: 'center',
 	            }}
-	            onPress={() => {
-	              null
-	            }}
-            >
-            	<MaterialCommunityIcons name="circle-outline" style={styles.capture_button}/>
-          	</TouchableHighlight>
+			>
+				<TouchableHighlight
+		            onPress={this.onPress.bind(this)}
+	            >
+	            	<View style={styles.captureButtonOuter} >
+	            		<View style={this.captureButtonInnerStyle()} />
+	            	</View>
+	          	</TouchableHighlight>
+          	</View>
           )
       }
 
 };
 
 const styles = StyleSheet.create({
-  capture_button: {
-    fontSize: 70, 
-    paddingBottom: 50,
-    color: 'white'
+  captureButtonOuter: {
+    height: 70, 
+    width: 70,
+    borderWidth: 5,
+    borderColor: 'white',
+    marginBottom: 20,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  captureButtonInner: {
+    height: 55, 
+    width: 55,
+    borderRadius: 50,
   },
 });
 
--- app/components/PostCamera.js
+++ app/components/PostCamera.js
@@ -1,58 +1,157 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import axios from "axios";
 import { Camera } from 'expo-camera';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import CaptureButton from './CaptureButton'
 
-const PostCamera = () => {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+class PostCamera extends Component {
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+  constructor(){
+    super();
+    this.state = {
+      hasCameraPermissions: null,
+      recording: false,
+      type: Camera.Constants.Type.front,
+      video: null,
+    }
+  }
 
-  if (hasPermission === null) {
-    return <View />;
+  async componentDidMount() {
+    this.getCameraPermissions()
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+
+  async getCameraPermissions(){
+    let { status } = await Camera.requestPermissionsAsync();
+    if (status === 'granted'){
+      this.setState({
+        hasCameraPermissions: status
+      })
+    }
   }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-          }}>
-          <TouchableHighlight
-            activeOpacity={0.6}
+
+  toggleRecording() {
+    const { recording } = this.state;
+
+    if (recording) {
+      this.stopRecording();
+    } else {
+      this.startRecording();
+    }
+  };
+
+   async startRecording(){
+    if (this.cam) {
+      this.setState({ recording: true }, async () => {
+        const video = await this.cam.recordAsync();
+        this.setState({ video });
+      });
+    }
+  };
+
+  async stopRecording() {
+    this.setState({ recording: false }, async () => {
+      await this.cam.stopRecording()
+    });
+  };
+
+  setCameraType(){
+    if (this.state.type === Camera.Constants.Type.back) {
+      this.setState({
+        type: Camera.Constants.Type.front
+      })
+    } else{
+      this.setState({
+        type: Camera.Constants.Type.back
+      })
+    }
+  }
+
+  submitVideo() {
+    console.log(this.state)
+    if (this.state.video) {
+      var uri = this.state.video.uri
+      var uriParts = uri.split('/')
+      var filename = uriParts[uriParts.length - 1]
+
+      var bodyFormData = new FormData();
+      bodyFormData.append('video', uri); 
+
+      // axios({
+      //   method: 'post',
+      //   url: 'http://127.0.0.1:5000/baguette/api/v1.0/posts',
+      //   data: bodyFormData,
+      //   headers: {'Content-Type': 'multipart/form-data' }
+      //   })
+      // .then(function (response) {
+      //   console.log(response);
+      // })
+      // .catch(function (response) {
+      //   console.log(response);
+      // });
+
+      this.setState({ 
+        video: null
+      });
+    }
+  }
+
+  render(){
+    if (this.state.hasCameraPermissions === null) {
+      return <View />;
+    }
+
+    if (this.state.hasCameraPermissions === false) {
+      return <Text>No access to camera</Text>;
+    }
+
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera 
+          style={{ flex: 1 }} 
+          type={this.state.type} 
+          ref={cam => (this.cam = cam)}
+        >
+          <View
             style={{
-              flex: 0.15,
-              alignSelf: 'flex-top',
-              alignItems: 'center',
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
             }}>
-            <MaterialCommunityIcons name="camera-switch" style={styles.flip}/>
+            <TouchableHighlight
+              activeOpacity={0.6}
+              style={{
+                flex: 0.15,
+                alignSelf: 'flex-top',
+                alignItems: 'center',
+              }}
+              onPress={() => { this.setCameraType()}}
+            >
+              <MaterialCommunityIcons name="camera-switch" style={styles.flip}/>
+            </TouchableHighlight>
+
+             {this.state.video && (
+              <TouchableHighlight
+                onPress={this.submitVideo.bind(this)}
+                style={{
+                  padding: 20,
+                  justifyContent: "center",
+                  textColor: 'white'
+                }}
+              >
+            <Text style={{ textAlign: "center" }}>save</Text>
           </TouchableHighlight>
+        )}
 
-          <CaptureButton />
+            <CaptureButton 
+              onPress={this.toggleRecording.bind(this)}
+            />
 
-        </View>
-      </Camera>
-    </View>
-  );
+          </View>
+        </Camera>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -60,11 +159,6 @@ const styles = StyleSheet.create({
     fontSize: 32, 
     marginTop: 10,
     marginLeft: 5, 
-    color: 'white'
-  },
-  capture_button: {
-    fontSize: 70, 
-    paddingBottom: 50,
     color: 'white'
   },
 });
