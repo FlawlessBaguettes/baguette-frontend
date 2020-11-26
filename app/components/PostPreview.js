cba7b09304ebed04b8b5e5ab1545022654b90110
shusnain <sibgatulhusnain@gmail.com>
2020-11-25 23:58:16 -0500

    Test sublime indentation to spaces

--- app/components/PostPreview.js
+++ app/components/PostPreview.js
@@ -6,34 +6,34 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import CameraStyle from '../styles/CameraStyle'
 
 class PostPreview extends Component {
-	constructor() {
-		super()
-	}
+  constructor() {
+    super()
+  }
 
-	render() {
-		var { uri } = this.props
+  render() {
+    var { uri } = this.props
 
-		return(
-			<View style={styles.container}>		
-				<StatusBar hidden='true' translucent='true' />
-   				<Video
-	 					source={{uri: uri}}
-		 				rate={1.0}
-				  	volume={1.0}
-						isMuted={false}
-						resizeMode={Video.RESIZE_MODE_COVER}
-						shouldPlay={true}
-						isLooping={true}
-				    style={styles.container}
-					/>
-			</View>
-		);
-	}
+    return(
+      <View style={styles.container}>   
+        <StatusBar hidden='true' translucent='true' />
+          <Video
+            source={{uri: uri}}
+            rate={1.0}
+            volume={1.0}
+            isMuted={false}
+            resizeMode={Video.RESIZE_MODE_COVER}
+            shouldPlay={true}
+            isLooping={true}
+            style={styles.container}
+          />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container:{
-		flex: 1
+  container:{
+    flex: 1
   },
 });
 
