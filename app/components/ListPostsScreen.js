d33dfa8bf1b04d255a2a7a2c1c86fd7d340d9185
shusnain <sibgatulhusnain@gmail.com>
2020-12-16 19:51:43 -0500

    Update app/components/ListPostsScreen.js
    Co-authored-by: Raghav Goswami <raghav.goswami576@gmail.com>

--- app/components/ListPostsScreen.js
+++ app/components/ListPostsScreen.js
@@ -37,7 +37,22 @@ const ListPostsScreen = ({ route, navigation }) => {
 
   return (
     <SafeAreaView style={styles.container}>
-    <Text onPress={() => {navigation.navigate('SignUpScreen')}}>Sign Up</Text>
+    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+        <Text
+          onPress={() => {
+            navigation.navigate("SignUpScreen");
+          }}
+        >
+          Sign Up
+        </Text>
+        <Text
+          onPress={() => {
+            navigation.navigate("LoginScreen");
+          }}
+        >
+          Login
+        </Text>
+      </View>
       <FlatList
         data={posts}
         renderItem={({ item }) => (
