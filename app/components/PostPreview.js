21eb3e1f295b5809df3121c3034e087e0da6a575
shusnain <sibgatulhusnain@gmail.com>
2020-12-24 15:21:01 -0500

    Convert PostPreviewControls to Functional Component

--- app/components/PostPreview.js
+++ app/components/PostPreview.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Video } from "expo-av";
 import { StatusBar } from "expo-status-bar";
